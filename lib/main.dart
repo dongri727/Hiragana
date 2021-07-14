c7c182bd0b85821489e21948a9ba259c15a002ee
happyturn <asap.happyturn@gmail.com>
2021-07-14 21:33:43 +0900

    入力したひらがなを、画面下部に表示する

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'main_model.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
       .then((_) => {runApp(MyApp())});
 }
 
 class MyApp extends StatelessWidget {
-
-  final items = ['よ','や','ら','ま','ば','ぱ','は','な','だ','た','ざ','さ','が','か','あ',
-    'ょ','ゃ','り','み','び','ぴ','ひ','に','ぢ','ち','じ','し','ぎ','き','い','　','ゆ','る',
-    'む','ぶ','ぷ','ふ','ぬ','づ','つ','ず','す','ぐ','く','う','　','ゅ','れ','め','べ','ぺ',
-    'へ','ね','で','て','ぜ','せ','げ','け','え','ん','わ','ろ','も','ぼ','ぽ','ほ','の','ど','と',
-    'ぞ','そ','ご','こ','お'];
+  final items = [
+    'よ',
+    'や',
+    'ら',
+    'ま',
+    'ば',
+    'ぱ',
+    'は',
+    'な',
+    'だ',
+    'た',
+    'ざ',
+    'さ',
+    'が',
+    'か',
+    'あ',
+    'ょ',
+    'ゃ',
+    'り',
+    'み',
+    'び',
+    'ぴ',
+    'ひ',
+    'に',
+    'ぢ',
+    'ち',
+    'じ',
+    'し',
+    'ぎ',
+    'き',
+    'い',
+    '　',
+    'ゆ',
+    'る',
+    'む',
+    'ぶ',
+    'ぷ',
+    'ふ',
+    'ぬ',
+    'づ',
+    'つ',
+    'ず',
+    'す',
+    'ぐ',
+    'く',
+    'う',
+    '　',
+    'ゅ',
+    'れ',
+    'め',
+    'べ',
+    'ぺ',
+    'へ',
+    'ね',
+    'で',
+    'て',
+    'ぜ',
+    'せ',
+    'げ',
+    'け',
+    'え',
+    'ん',
+    'わ',
+    'ろ',
+    'も',
+    'ぼ',
+    'ぽ',
+    'ほ',
+    'の',
+    'ど',
+    'と',
+    'ぞ',
+    'そ',
+    'ご',
+    'こ',
+    'お'
+  ];
   @override
   Widget build(BuildContext context) {
     final title = 'なまえをかきましょう';
+    String inputText = '';
 
     return MaterialApp(
       title: title,
@@ -25,16 +99,31 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(title),
         ),
-        body: GridView.count(
-
-          crossAxisCount: 15,
-
-          children: List.generate(75, (index) {
-            return Center(
-              child: Text(
-                '${items[index]}',
-                style: Theme.of(context).textTheme.headline5,
-              ),
+        body: ChangeNotifierProvider<MainModel>(
+          create: (_) => MainModel(),
+          child: Consumer<MainModel>(builder: (_, model, __) {
+            return Column(
+              children: [
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 15,
+                    children: List.generate(75, (index) {
+                      return Center(
+                        child: TextButton(
+                          onPressed: () {
+                            model.appendText('${items[index]}');
+                          },
+                          child: Text('${items[index]}'),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+                Text(
+                  model.getText(),
+                  style: TextStyle(fontSize: 24, color: Colors.indigo),
+                ),
+              ],
             );
           }),
         ),
--- lib/main_model.dart
+++ lib/main_model.dart
@@ -2,4 +2,13 @@ import 'package:flutter/cupertino.dart';
 
 class MainModel extends ChangeNotifier {
   String _inputText = "";
+
+  void appendText(String val) {
+    this._inputText = this._inputText + val;
+    notifyListeners();
+  }
+
+  String getText() {
+    return this._inputText;
+  }
 }
